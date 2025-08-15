05e14a7534f3acd306a170d56cc52eb7af91958e
loveyou654987654-droid <loveyou654987654@gmail.com>
2025-08-16 01:14:48 +0530

    Update KRISHNA-LOVE.js

--- models/commands/KRISHNA-LOVE.js
+++ models/commands/KRISHNA-LOVE.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(220, 220), 230, 155).composite(circleTwo.resize(220, 220), 800,150);
+    batgiam_img.composite(circleOne.resize(220, 220), 235, 147).composite(circleTwo.resize(220, 220), 805,147);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
